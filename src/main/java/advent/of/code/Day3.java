204f820cc1f38fd3d219167e60ded1423d6a27c6
HyperFrankie <lenardvdmaas@hotmail.nl>
2021-12-04 14:58:29 +0100

    Day3 part 2

--- src/main/java/advent/of/code/Day3.java
+++ src/main/java/advent/of/code/Day3.java
@@ -1,30 +1,30 @@
 package advent.of.code;
 
 import java.util.Arrays;
-import java.util.Map;
 
 import static advent.of.code.Utils.getResourceFileAsString;
-import static advent.of.code.Utils.splitInputToMap;
 
 public class Day3 {
 
 	private static String[] lines;
 
 	public static void main(String[] args) {
-		String input = getResourceFileAsString("day2.txt");
+		String input = getResourceFileAsString("day3.txt");
 		if(input != null && !input.equals("")) {
 			lines = input.split("\r\n");
-			part1();
-//			part2();
+//			part1();
+			part2();
 		}
 	}
 
 	public static void part1() {
-
 		int[] counts = {0,0,0,0,0,0,0,0,0,0,0,0};
 		for (String l : lines) {
 			char[] chars = l.toCharArray();
-			for (int i = 0; i < chars.length; i++) counts[i] += chars[i] * 2 - 1;
+			for (int i = 0; i < chars.length; i++) {
+				System.out.println(chars[i]);
+				counts[i] += Integer.parseInt(String.valueOf(chars[i])) * 2 - 1;
+			}
 		}
 		byte[] result1 = {0,0,0,0,0,0,0,0,0,0,0,0};
 		byte[] result2 = {0,0,0,0,0,0,0,0,0,0,0,0};
@@ -40,7 +40,36 @@ public class Day3 {
 		System.out.println(arrayPrint1(counts) + ", " + arrayPrint(result1) + ", " + arrayPrint(result2));
 		System.out.println(resultNum1 + ", " + resultNum2);
 		System.out.println(resultNum1 * resultNum2);
-		
+	}
+
+	public static void part2() {
+		String[] newList = lines.clone();
+		Arrays.sort(newList);
+		System.out.println(findNum(newList, 0) * findNum(newList, 1));
+	}
+
+	private static int findNum(String[] list, int mode) {
+		int bit = 0;
+		while (list.length != 1 && bit < 12) {
+			int index = (int) Math.ceil((list.length - 1) / 2d), keep = Integer.parseInt(list[index].substring(bit, bit + 1));
+			if (mode == 1) keep = 1 - keep;
+			System.out.println("end index: " + (list.length - 1));
+			System.out.println("half: " + index);
+			System.out.println("keep: " + keep);
+			int i = 0;
+			while (list[i].charAt(bit) == '0') i++;
+			i += keep;
+			System.out.println("newIndex: " + i);
+			list = Arrays.copyOfRange(list, keep * i, i + keep * (list.length - i));
+			bit++;
+		}
+		String resultBytes = list[0];
+		int result = 0;
+		for (int i = 0, j = resultBytes.length(); i < resultBytes.length(); i++, j--) {
+			result += Integer.parseInt(resultBytes.substring(i, i + 1)) * Math.pow(2, j);
+		}
+		System.out.println(resultBytes + ": " + result);
+		return result;
 	}
 
 	public static String arrayPrint(byte[] bitArray) {
@@ -57,10 +86,6 @@ public class Day3 {
 			result += b + ",";
 		}
 		return result;
-	}
-
-	public static void part2() {
-
 	}
 
 }
